d2cacae666847dda120d07c6e99cd865ca67864e
pedrocpacheco <cpedropacheco@hotmail.com>
2023-07-24 22:01:57 -0300

    comments

--- Fundamentos/src/index.js
+++ Fundamentos/src/index.js
@@ -2,6 +2,21 @@ const express = require("express");
 
 const app = express();
 
+/**
+ * METODOS HTTP: 
+ * GET - Buscar uma informação dentro do servidor
+ * POST - Inserir uma informação no servidor
+ * PUT - Alterar uma informação no servidor
+ * PATCH - Alterar uma informação especifica
+ * DELETE - Deletar uma informação do servidor
+ */
+
+/**
+ * Tipos de Parametro:
+ * 
+ * Route Params => Identificar um recurso /
+ */
+
 app.get("/courses", (req, res) => {
     return res.json(["Curso 1", "Curso 2", "Curso 3"]);
 });
