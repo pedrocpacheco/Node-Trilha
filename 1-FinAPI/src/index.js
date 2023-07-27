58372f0ec6e79bd87536ad1e823c2f73a0cf46a3
pedrocpacheco <cpedropacheco@hotmail.com>
2023-07-27 16:18:04 -0300

    feat: POST creating a new account

--- 1-FinAPI/src/index.js
+++ 1-FinAPI/src/index.js
@@ -1,5 +1,33 @@
 const express = require("express")
+const { v4: uuidv4 } = require("uuid") 
 
 const app = express()
+
+app.use(express.json())
+
+const costumers = [];
+
+/**
+ * CONTA:
+ * CPF - String
+ * Name - String
+ * ID - uuid
+ * statement []
+ */
+app.post("/accounts", (req, res) => {
+    const { cpf, name } = req.body;
+
+    const id = uuidv4();
+
+    const costumer =  {
+        cpf,
+        name,
+        id,
+        statement: []
+    }
+
+    costumers.push(costumer);
+    return res.status(201).send(costumer);
+})
 
 app.listen(3333)
