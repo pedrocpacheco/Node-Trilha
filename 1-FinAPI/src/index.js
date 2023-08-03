c69c2d60a397973e4a1a1521edab4f91e7d60bf3
Logon Aluno <logonrmlocal@fiap.com.br>
2023-08-03 11:20:52 -0300

    feat: post method validating cpf

--- 1-FinAPI/src/index.js
+++ 1-FinAPI/src/index.js
@@ -7,27 +7,25 @@ app.use(express.json())
 
 const costumers = [];
 
-/**
- * CONTA:
- * CPF - String
- * Name - String
- * ID - uuid
- * statement []
- */
 app.post("/accounts", (req, res) => {
     const { cpf, name } = req.body;
 
-    const id = uuidv4();
+    const constumerAlreadyExists = costumers.some((costumer) => costumer.cpf === cpf); // ! Verifica se há account com cpf =
+
+    if(constumerAlreadyExists){
+        return response.status(400).json({ error: "Costumer Already Exits" }) // ? Se existir, devolve esse erro
+    }
 
     const costumer =  {
         cpf,
         name,
-        id,
+        id: uuidv4(),
         statement: []
     }
 
     costumers.push(costumer);
     return res.status(201).send(costumer);
 })
+
 
 app.listen(3333)
